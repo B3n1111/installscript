e2048bf8d64f8f6adf8c969d2a48344e5efacce5
B3n1111 <b3n1111.gaming@web.de>
2025-08-24 18:52:25 +0200

    First Version of my installscript

---
+++ installscript.ps1
@@ -0,0 +1,12 @@
+winget install Microsoft.VCRedist.2015+.x64 --silent --accept-package-agreements --accept-source-agreements --scope machine
+winget install Microsoft.VCRedist.2015+.x86 --silent --accept-package-agreements --accept-source-agreements --scope machine
+
+
+winget install Mozilla.Firefox.de --silent --accept-package-agreements --accept-source-agreements --scope machine
+winget install DominikReichl.KeePass --silent --accept-package-agreements --accept-source-agreements --scope machine
+winget install Valve.Steam --silent --accept-package-agreements --accept-source-agreements --scope machine
+winget install Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements --scope user
+winget install Discord.Discord --silent --accept-package-agreements --accept-source-agreements
+winget install Mozilla.Thunderbird.de --silent --accept-package-agreements --accept-source-agreements --scope machine
+winget install SteelSeries.GG --silent --accept-package-agreements --accept-source-agreements --scope machine
+winget install Obsidian.Obsidian --silent --accept-package-agreements --accept-source-agreements
